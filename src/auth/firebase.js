09ad5148fc177d5cddebcf3d5664a5ea280724fc
shree0007 <shree.bhusal07@gmail.com>
2023-09-22 12:59:14 +0300

    added firebase config file

---
+++ src/auth/firebase.js
@@ -0,0 +1,65 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/firebase-auth";
+import { addDoc, collection, getFireStore } from "firebase/firestore";
+
+// import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDiM_SHPytbdPW7hf-EiXEqES1SmlihLMs",
+    authDomain: "countriesapp-2a813.firebaseapp.com",
+    projectId: "countriesapp-2a813",
+    storageBucket: "countriesapp-2a813.appspot.com",
+    messagingSenderId: "455481108671",
+    appId: "1:455481108671:web:d18e3aa9ac20ea97ae3e0a",
+    measurementId: "G-CMHK228FWD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+const auth = getAuth(app)
+const db = getFireStore(app)
+
+//login in
+const loginWithEmailAndPassword = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password)
+    }
+    catch (err) {
+        console.log(err)
+        alert(err.message)
+    }
+}
+
+//create user
+const registerWithEmalAndPassword = async (name, email, password) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password)
+        const user = res.user
+        await addDoc(collection(db, 'users'), {
+            uid: user.uid,
+            name,
+            authProvider: 'local',
+            email,
+        })
+    }
+    catch (err) {
+        console.log(err)
+        alert(err.message)
+    }
+}
+
+//logout
+const logout = () => {
+    signOut(auth)
+}
+
+
+export { auth, db, loginWithEmailAndPassword, registerWithEmalAndPassword, logout };
