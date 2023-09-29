c0cb3245ddb589b211eabb361121d47abbae647e
shree0007 <shree.bhusal07@gmail.com>
2023-09-29 14:31:07 +0300

    created favouritesSlice.js

---
+++ src/features/countries/favouritesSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const favourites = localStorage.getItem('favourites') !== null ? JSON.parse(localStorage.getItem('favourites')) : []
+
+export const favouritesSlice = createSlice({
+    name: 'favourites',
+    initialState: {
+        favourites,
+    },
+    reducers: {
+        addFavourite(state, action) {
+            // The line below is not necessary but can be useful as a check to see if localstorage favourit already exists
+            if (state.favourites.some(fav => fav === action.payload)) state.favourites = [...state.favourites]
+            state.favourites = [...state.favourites, action.payload]
+            localStorage.setItem('favourites', JSON.stringify(state.favourites))
+        },
+        removeFavourite(state, action) {
+            const newArray = [...state.favourites]
+            newArray.splice(newArray.findIndex(e => e === action.payload), 1)
+            state.favourites = [...newArray]
+        },
+        clearFavourite(state, action) {
+            localStorage.removeItem('favourites')
+            state.favourites = []
+        }
+    }
+});
+
+export default favouritesSlice.reducer
