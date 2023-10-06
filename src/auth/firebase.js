a7e9c520631b47e3db45fc7dc364f82733a07ee6
shree0007 <shree.bhusal07@gmail.com>
2023-10-06 18:15:20 +0300

    added addFavourite, removeFavourite, and clearFavourite functions in firebase.js

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, getDocs, getFirestore, query, where } from "firebase/firestore";
 
 // import { getAnalytics } from "firebase/analytics";
 
@@ -60,6 +60,45 @@ const registerWithEmailAndPassword = async (name, email, password) => {
 const logout = () => {
     signOut(auth)
 }
+
+export const addFavouriteToFirebase = async (uid, name) => {
+    try {
+        await addDoc(collection(db, `users/${uid}/favourites`), { name });
+        console.log("Favourite added to Firebase database");
+    } catch (err) {
+        console.error("Error adding favourite to Firebase database: ", err);
+    }
+};
+
+export const removeFavouriteFromFirebase = async (uid, name) => {
+    try {
+        if (!name) {
+            console.error("Error removing favourite from Firebase database: name parameter is undefined");
+            return;
+        }
+        const q = query(collection(db, `users/${uid}/favourites`), where("name", "==", name));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            deleteDoc(doc.ref);
+            console.log("Favourite removed from Firebase database");
+        });
+    } catch (err) {
+        console.error("Error removing favourite from Firebase database: ", err);
+    }
+};
+
+export const clearFavouritesFromFirebase = async (uid) => {
+    try {
+        const q = query(collection(db, `users/${uid}/favourites`));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            deleteDoc(doc.ref);
+            console.log("Favourites removed from Firebase database");
+        });
+    } catch (err) {
+        console.error("Error removing favourites from Firebase database: ", err);
+    }
+};
 
 
 export { auth, db, loginWithEmailAndPassword, registerWithEmailAndPassword, logout };
